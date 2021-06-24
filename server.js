2cdba64fbbcbc3e7eaba6ea5eaccc852616d1021
amanosacrous <andreumanosa@gmail.com>
2021-06-24 11:33:53 +0200

    nodejs server set heaers

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const path = require('path');
 
 const app = express();
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://covid19-api.weedmark.systems/api/v1/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/COVID19Dashboard'));
 
